e50283e62e87d634614caf78bf0ffe6b58fbd7fa
Adithya Sandew <92386905+adithyasandew@users.noreply.github.com>
2024-01-26 03:45:15 +0530

    Update Search.jsx

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -131,9 +131,9 @@ export default function Search() {
               id='category'
             >
               <option value='uncategorized'>Uncategorized</option>
-              <option value='reactjs'>React.js</option>
-              <option value='nextjs'>Next.js</option>
-              <option value='javascript'>JavaScript</option>
+              <option value='reactjs'>Web Development</option>
+              <option value='nextjs'>Linux</option>
+              <option value='javascript'>Ethical Hacking</option>
             </Select>
           </div>
           <Button type='submit' outline gradientDuoTone='purpleToPink'>
