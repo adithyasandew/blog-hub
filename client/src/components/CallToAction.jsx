b9fb3fb734548232242d4d9304350bb36560ed88
Adithya Sandew <92386905+adithyasandew@users.noreply.github.com>
2024-01-26 01:46:15 +0530

    Advertisment

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,19 +5,19 @@ export default function CallToAction() {
     <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
         <div className="flex-1 justify-center flex flex-col">
             <h2 className='text-2xl'>
-                Want to learn more about JavaScript?
+                Create Your Dream Website - Professional Web Development Service
             </h2>
             <p className='text-gray-500 my-2'>
-                Checkout these resources with 100 JavaScript Projects
+                Are you ready to bring your dream website to life? Look no further! I am a seasoned web developer with a passion for turning visions into stunning, functional websites. Whether you need a personal blog, a sleek portfolio, or a robust e-commerce platform, I've got you covered.
             </p>
-            <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
-                <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
-                    100 JavaScript Projects
+            <Button gradientDuoTone='pinkToOrange' className='rounded-tl-xl rounded-bl-none'>
+                <a href="https://https://wa.me/+94773916022?text=Hey" target='_blank' rel='noopener noreferrer'>
+                    Contact Me
                 </a>
             </Button>
         </div>
         <div className="p-7 flex-1">
-            <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+            <img src="https://entrepreneurhandbook.co.uk/wp-content/uploads/2019/10/Website-builder.jpg.webp" />
         </div>
     </div>
   )
