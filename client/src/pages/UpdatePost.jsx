7d141c9aa2e2891381d5111e51f5fcb7b648d7f4
Adithya Sandew <92386905+adithyasandew@users.noreply.github.com>
2024-01-26 23:10:40 +0530

    Update UpdatePost.jsx

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -130,9 +130,9 @@ export default function UpdatePost() {
             value={formData.category}
           >
             <option value='uncategorized'>Select a category</option>
-            <option value='javascript'>JavaScript</option>
-            <option value='reactjs'>React.js</option>
-            <option value='nextjs'>Next.js</option>
+            <option value='javascript'>Web Development</option>
+            <option value='reactjs'>Linux</option>
+            <option value='nextjs'>Ethical Hacking</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
