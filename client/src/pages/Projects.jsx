2839bf3165380cbbe99e5a4f3e057ee72b2be663
Adithya Sandew <92386905+adithyasandew@users.noreply.github.com>
2024-01-27 12:31:10 +0530

    Update Projects.jsx

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,6 +1,6 @@
 import CallToAction from '../components/CallToAction';
 
-export default function Service() {
+export default function Projects() {
   return (
     <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
       <h1 className='text-3xl font-semibold'>Services</h1>
