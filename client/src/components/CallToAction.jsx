79809bdb37f3d8bae26bbdb98c9084affc475bbf
Adithya Sandew <92386905+adithyasandew@users.noreply.github.com>
2024-01-27 13:56:44 +0530

    Update CallToAction.jsx

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -10,11 +10,11 @@ export default function CallToAction() {
             <p className='text-gray-500 my-2'>
                 Are you ready to bring your dream website to life? Look no further! I am a seasoned web developer with a passion for turning visions into stunning, functional websites. Whether you need a personal blog, a sleek portfolio, or a robust e-commerce platform, I've got you covered.
             </p>
-            <Button gradientDuoTone='pinkToOrange' className='rounded-tl-xl rounded-bl-none'>
-                <a href="https://wa.me/+94787334812" target='_blank' rel='noopener noreferrer'>
+            <a href="https://wa.me/+94787334812" target='_blank' rel='noopener noreferrer'>    
+                <Button gradientDuoTone='pinkToOrange' className='rounded-tl-xl rounded-bl-none'>
                     Contact Me
-                </a>
-            </Button>
+                </Button>
+            </a>
         </div>
         <div className="p-7 flex-1">
             <img src="https://entrepreneurhandbook.co.uk/wp-content/uploads/2019/10/Website-builder.jpg.webp" />
