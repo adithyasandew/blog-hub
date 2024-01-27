52dae499a232ee32b61a86753939ad305e3e6052
Adithya Sandew <92386905+adithyasandew@users.noreply.github.com>
2024-01-27 13:00:11 +0530

    Update and rename Projects.jsx to Services.jsx

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -1,6 +1,6 @@
 import CallToAction from '../components/CallToAction';
 
-export default function Projects() {
+export default function Project() {
   return (
     <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
       <h1 className='text-3xl font-semibold'>Services</h1>
